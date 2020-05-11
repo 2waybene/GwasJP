668dd7d852a03a74055486b3be0dd4d295bfc4a3
2waybene <jianying.li@gmail.com>
2020-05-11 10:36:20 -0400

    added bin dir

--- GwasJP/accord/accordAnalysis.py
+++ GwasJP/accord/accordAnalysis.py
@@ -25,7 +25,6 @@ def modelStep1 (filepath, phenotype = "pheno_data_rhtn.txt", phenoname = "RHTN")
     ## prepare a file pheontypes.txt
     phenotypes = filepath + "/" + "phenotypes.txt"
     f = open(phenotypes, 'w')
-
     f.write(phenoname)
     f.close()
 
